c0cd644d89e7cff03661fecf8bd15fee0264a119
Sergio Ojeda <sojeda@inameh.gob.ve>
2016-05-31 10:58:38 -0400

    Se elimina el metodo edit por la misma razon, es una API asi que no muestra el formulario

--- app/Http/Controllers/Api/NoteController.php
+++ app/Http/Controllers/Api/NoteController.php
@@ -41,17 +41,7 @@ class NoteController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
+  
     /**
      * Update the specified resource in storage.
      *
